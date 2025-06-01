dd4455c5fe1cc5a039208c78fd2a371e899dfb72
Bastien Gaffet <bastien.gaffet2007@gmail.com>
2025-06-01 10:39:18 +0200

    main loop and camera separation

--- python/connect4/main.py
+++ python/connect4/main.py
@@ -1,6 +1,6 @@
 from core import init_game
-from camera.camera import camera_loop
+from game_loop import run_game_loop
 
 if __name__ == "__main__":
     game_state = init_game()
-    camera_loop(game_state)
+    run_game_loop(game_state)
