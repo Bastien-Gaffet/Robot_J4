35981c97ca133aeaca37bd452d998092b62047da
Bastien Gaffet <bastien.gaffet2007@gmail.com>
2025-06-08 23:39:54 +0200

    Update connection and data transfert to db

--- python/j4_connect4/connect4_robot_j4/game_data/fire_base.py
+++ python/j4_connect4/connect4_robot_j4/game_data/fire_base.py
@@ -2,11 +2,11 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import datetime
 import os
-from game_state import GameState
-from constants import MINIMAX_DEPTH
+from connect4_robot_j4.game_state import GameState
+from connect4_robot_j4.constants import MINIMAX_DEPTH
 
 # 1. Initialization of Firebase Admin SDK
-key_path = os.environ.get("FIREBASE_CRED", "connect4_robot_j4/game_data/serviceAccountKey.json")
+key_path = os.environ.get("FIREBASE_CRED")
 cred = credentials.Certificate(str(key_path))
 firebase_admin.initialize_app(cred)
 
@@ -21,6 +21,7 @@ def get_game_data(game_state: GameState):
         "game_id": game_state.game_id,
         "timestamp": datetime.datetime.now(datetime.timezone.utc).isoformat(),
         "duration_seconds": (game_state.game_end_time - game_state.game_start_time).total_seconds(),
+        "first_player": game_state.first_player,
         "moves": game_state.moves,
         "winner": game_state.winner,
         "player_pseudo": game_state.player_pseudo,
@@ -34,5 +35,5 @@ def send_game_data(game_state: GameState):
     game_data = get_game_data(game_state)
     
     # 3. Sending to the "games" collection
-    db.collection("games").add(game_data)
+    db.collection("games").document(game_data["game_id"]).set(game_data)
     print("Game successfully sent to Firebase!")
